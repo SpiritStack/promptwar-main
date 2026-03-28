74ce4c4343c31d5b019a2e62f8bfdc807ad5cf62
Vikash Sharma <vikash.sharma@in.ibm.com>
2026-03-28 15:51:35 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_ID=$(gcloud config get-value project)
-API_KEY=$(grep GEMINI_API_KEY .env.local | cut -d '=' -f 2)
+API_KEY=$(grep '^GEMINI_API_KEY=' .env.local | cut -d '=' -f 2)
 
 echo "Deploying to project: $PROJECT_ID"
 echo "Building and deploying from source..."
